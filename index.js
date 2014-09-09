91943baec79080412a2e83b0835b2ad4c0d4f391
App Academy Student <student@appacademy.io>
2014-09-09 15:54:15 -0400

    initial commit, working chat with alias and type status features

---
+++ index.js
@@ -0,0 +1,29 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.get('/', function(req, res){
+  res.sendFile('/Users/appacademy/Desktop/chat-example/index.html');
+});
+
+io.on('connection', function(socket){
+  console.log("a user connected");
+
+  socket.on('type status', function(data){
+    socket.broadcast.emit('type status', data)
+  })
+
+  socket.on('chat message', function(msg){
+    socket.broadcast.emit('chat message', msg)
+  })
+
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+    io.emit('chat message', "user disconnected")
+  })
+});
+
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
